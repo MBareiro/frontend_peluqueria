2a75c0633f6a23d0e231ae0cddb112e04e0be7b1
MBareiro <58647378+MBareiro@users.noreply.github.com>
2023-09-08 21:50:20 -0300

    Creacion de la pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    CarouselComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
