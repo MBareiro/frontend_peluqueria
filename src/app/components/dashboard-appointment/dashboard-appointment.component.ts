8064eedf08c3a942eeb68adc9165d3c8af23e060
MBareiro <58647378+MBareiro@users.noreply.github.com>
2023-09-22 15:25:56 -0300

    Gestion de turnos y lista de turnos actualizada

--- src/app/components/dashboard-appointment/dashboard-appointment.component.ts
+++ src/app/components/dashboard-appointment/dashboard-appointment.component.ts
@@ -5,29 +5,27 @@ import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-dashboard-appointment',
   templateUrl: './dashboard-appointment.component.html',
-  styleUrls: ['./dashboard-appointment.component.css']
+  styleUrls: ['./dashboard-appointment.component.css'],
 })
 export class DashboardAppointmentComponent {
   private breakpointObserver = inject(BreakpointObserver);
 
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
-
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  showCreateAppointment = true;
+  showListAppointment = false;
+  /* 
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );  
+ */
+  showListAppointmentComponent() {
+    this.showCreateAppointment = false;
+    this.showListAppointment = true;
+  }
+  showCreateAppointmentComponent() {
+    this.showCreateAppointment = true;
+    this.showListAppointment = false;
+  }
 }
